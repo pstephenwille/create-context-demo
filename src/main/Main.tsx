79e505ff508eef6167f9f8f19ac9b0eb60222a5c
stephen.wille <stephen.wille@climate.com>
2020-02-18 09:33:59 -0800

    Added Cypress e2e tests

--- src/main/Main.tsx
+++ src/main/Main.tsx
@@ -6,6 +6,7 @@ import Home2 from "../mm-tool-2/home/Home2";
 
 export default (props) => {
     const {activeTool} = useContext(ActiveToolContext);
+    console.log('...props history ', props.history);
     const switchTool = () => {
         if (activeTool.match(/service/i)) {
             return (<Home history={props.history}/>);
